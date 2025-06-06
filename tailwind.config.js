2f0bacc2c1b5f47a2014a1347d00c23f139e8e5b
gustalmeidaa <gustavoheubel@gmail.com>
2025-06-05 23:51:48 -0300

    Testando alterações para nginx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  // <-- importante
-  ],
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
